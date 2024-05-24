9172e1e3c43bd7b48db74943351122ed65c875e2
Tarun Bisht <bishttarun2002@gmail.com>
2024-05-24 22:51:55 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -46,8 +46,7 @@ const About = () => {
         <h3 className='subhead-text'>Work Experience.</h3>
         <div className='mt-5 flex flex-col gap-3 text-slate-500'>
           <p>
-            I've worked with all sorts of companies, leveling up my skills and
-            teaming up with smart people. Here's the rundown:
+            I am a fresher, leveling up my skills and teaming up with smart people. Here's the rundown:
           </p>
         </div>
 
