590b62d5c32fd2918dd8398be111b6755467876d
Tarun Bisht <bishttarun2002@gmail.com>
2024-05-24 22:52:25 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
 
       <div className='footer-container'>
         <p>
-          © 2023 <strong>Tarun Bisht</strong>. All rights reserved.
+          © 2024 <strong>Tarun Bisht</strong>. All rights reserved.
         </p>
 
         <div className='flex gap-3 justify-center items-center'>
