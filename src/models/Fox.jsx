7cb99289b1ae474e4342461b33471cc62445cee4
Tarun Bisht <bishttarun2002@gmail.com>
2024-07-07 00:33:01 +0530

    Update Fox.jsx

--- src/models/Fox.jsx
+++ src/models/Fox.jsx
@@ -1,14 +1,3 @@
-/**
- * IMPORTANT: Loading glTF models into a Three.js scene is a lot of work.
- * Before we can configure or animate our model’s meshes, we need to iterate through
- * each part of our model’s meshes and save them separately.
- *
- * But luckily there is an app that turns gltf or glb files into jsx components
- * For this model, visit https://gltf.pmnd.rs/
- * And get the code. And then add the rest of the things.
- * YOU DON'T HAVE TO WRITE EVERYTHING FROM SCRATCH
- */
-
 import React, { useRef, useEffect } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
 
@@ -20,10 +9,8 @@ const Fox = ({ currentAnimation, ...props }) => {
   const { nodes, materials, animations } = useGLTF(scene);
   const { actions } = useAnimations(animations, group);
 
-  // This effect will run whenever the currentAnimation prop changes
   useEffect(() => {
     Object.values(actions).forEach((action) => action.stop());
-
     if (actions[currentAnimation]) {
       actions[currentAnimation].play();
     }
