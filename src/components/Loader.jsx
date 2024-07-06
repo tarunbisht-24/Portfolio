5e864b1298d2b05ac9c1af1c0e76a5eca9aeb9cd
Tarun Bisht <bishttarun2002@gmail.com>
2024-07-06 20:01:44 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,14 +1,13 @@
-import { Html } from "@react-three/drei"
+import { Html } from "@react-three/drei";
 
 export const Loader = () => {
   return (
     <Html>
-    <div className="flex justify-center items-center">
-      <div className="w-20 h-20 border-2 border-opacity-20
-      border-blue-500 border-t-blue-500 rounded-full animate-spin"></div>
-    </div>
+      <div className="flex justify-center items-center">
+        <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin"></div>
+      </div>
     </Html>
-  )
-}
+  );
+};
 
 export default Loader;
