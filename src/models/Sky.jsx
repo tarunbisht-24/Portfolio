0570f164ffba994e58ca77993072f8645c2d7777
Tarun Bisht <bishttarun2002@gmail.com>
2024-07-07 00:32:52 +0530

    Update Sky.jsx

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -9,9 +9,6 @@ const Sky = ({ isRotating }) => {
   const sky = useGLTF(skyScene);
   const skyRef = useRef();
 
-  // Note: Animation names can be found on the Sketchfab website where the 3D model is hosted.
-  // It ensures smooth animations by making the rotation frame rate-independent.
-  // 'delta' represents the time in seconds since the last frame.
   useFrame((_, delta) => {
     if (isRotating) {
       skyRef.current.rotation.y += 0.25 * delta; // Adjust the rotation speed as needed
@@ -20,8 +17,6 @@ const Sky = ({ isRotating }) => {
 
   return (
     <mesh ref={skyRef}>
-      // use the primitive element when you want to directly embed a complex 3D
-      model or scene
       <primitive object={sky.scene} />
     </mesh>
   );
